ca73ae8ad03bb555fe78e6b1dd156677fd71dc9c
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-11-17 00:20:16 +0600

    Footer Added

--- resources/views/components/mobile-navlink.blade.php
+++ resources/views/components/mobile-navlink.blade.php
@@ -1,6 +1,6 @@
 @props(['active' => false])
 
-<a class="{{ $active ? 'block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white' : 'block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white' }}" 
+<a class="{{ $active ? 'block rounded-md bg-teal-600 px-3 py-2 text-base font-medium text-white' : 'block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-white hover:text-teal-800' }}" 
    aria-current="{{ $active ? 'page' : 'false' }}"
    {{ $attributes }}>
    {{ $slot }}
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -23,7 +23,7 @@
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="shrink-0">
-                <img class="size-12" src="{{ asset('logos/kuetlogo.png') }}" alt="Your Company">
+                <img class="size-12" src="{{ asset('logos/kuetlogo.png') }}" alt="KUET">
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
@@ -155,6 +155,37 @@
       </div>
     </main>
   </div>
+
+  
+
+  <footer class="bg-white rounded-lg shadow dark:bg-teal-800 m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="{{ asset('logos/kuetlogo.png') }}" class="h-12" alt="KUET" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Central Medical Center</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-white">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-white">© 2024 <a href="https://github.com/faiyaz103/KUETMedicalCenter" target="_blank" class="hover:underline">System Development Project (CSE 3200)</a></span>
+    </div>
+</footer>
+
+
   
 </body>
 </html>
