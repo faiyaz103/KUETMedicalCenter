210519c6438d986bef3e10fc90408ddfd4382c13
Tahmid <tahmid2007097@stud.kuet.ac.bd>
2024-12-26 19:59:32 +0600

    Initial commit

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class RegisteredUserController extends Controller
+{
+    public function create(){
+        return view ('auth.register');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name'  => ['required'],
+            'email'      => ['required', 'email'],
+            'password'   => ['required', Password::min(6), 'confirmed']
+        ]);
+//create the user
+        $user = User::create($attributes);
+
+//login
+        Auth::login($user);
+
+//redirect anywhete
+        return redirect('/');
+    }
+
+}
