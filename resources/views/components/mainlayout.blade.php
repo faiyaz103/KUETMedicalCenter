b04d395595df4270c6bd9b973802d7da19a0d016
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-01-03 13:49:07 +0600

    Modified

--- resources/views/components/mainlayout.blade.php
+++ resources/views/components/mainlayout.blade.php
@@ -9,7 +9,7 @@
     <title>{{ $title }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="h-full">
+<body class="h-full bg-white">
     <!-- Include Header -->
     <x-header />
 
