1a30c84f1bfcd77ccab53122b605c162e769540b
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-03-14 22:11:22 +0600

    Email Verification Added

--- routes/web.php
+++ routes/web.php
@@ -53,11 +53,13 @@ Route::get('/home', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 // -----------------Profile--------------------
+// Profile View and Delete without verification
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+// Profile Update with Verification
+Route::patch('/profile', [ProfileController::class, 'update'])->middleware(['auth', 'verified'])->name('profile.update');
 
 // ################ AUTHENTICATED USER ###################
 
